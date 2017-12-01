5ce6abf0a4cea7fe4038a911397184502d74848c
adamszekely <szekelyadam93@gmail.com>
2017-12-01 11:27:00 +0100

    fixed map, it shows your location and it puts marker at a given address

--- app/src/main/java/com/example/adam/eventhunter/MainActivity.java
+++ app/src/main/java/com/example/adam/eventhunter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.adam.eventhunter;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
@@ -30,8 +32,10 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements android.location.LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+import java.io.IOException;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements android.location.LocationListener, OnMapReadyCallback {
     private GoogleMap map;
     private TextView textView;
     protected GoogleApiClient mGoogleApiClient;
@@ -61,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements android.location.
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000  * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 1; // 1 minute
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -78,155 +82,49 @@ public class MainActivity extends AppCompatActivity implements android.location.
         super.onCreate(savedInstanceState);
         createLocationRequest();
         setContentView(R.layout.activity_main);
-        //mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API).addConnectionCallbacks(this).addOnConnectionFailedListener(this)
-                .build();
         mContext = this.getApplicationContext();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         currentLocation = getLocation();
-        /*mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                for (Location location : locationResult.getLocations()) {
-                    currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    CameraPosition target = CameraPosition.builder().target(currentLocation).zoom(18).build();
-                    map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-                    map.addMarker(new MarkerOptions()
-                            .position(currentLocation)
-                            .title("Here you are"));
-                }
-            };
-        };*/
-    }
-
-    protected void onStart() {
-        super.onStart();
-
-        if (mGoogleApiClient.isConnected()) {
-            startLocationUpdates();
-        }
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle connectionHint) {
-        currentLocation = getLocation();
-        updateUI();
-        startLocationUpdates();
-       /* mLocationRequest =LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000);
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},42);
-        }
-        else
-        {
-            mLastLocation=LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if(mLastLocation!=null)
-            {
-                newLocation=new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-                CameraPosition target= CameraPosition.builder().target(newLocation).zoom(18).build();
-                map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-                map.addMarker(new MarkerOptions()
-                        .position(newLocation)
-                        .title("Here you are"));
-                onLocationChanged(mLastLocation);
-            }
-        }*/
-    }
-
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 42);
-        }
-
-        /*if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
-            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-                    mLocationCallback,
-                    null);
-        }*/
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        // mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        currentLocation = getLocation();
-        updateUI();
 
-    }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
 
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 
     public void updateUI() {
         if (currentLocation != null) {
-            newLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            CameraPosition target = CameraPosition.builder().target(newLocation).zoom(18).build();
+            newLocation = new LatLng(getLocation().getLatitude(), getLocation().getLongitude());
+            CameraPosition target = CameraPosition.builder().target(newLocation).zoom(14).build();
             map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
-            map.addMarker(new MarkerOptions()
-                    .position(newLocation)
-                    .title("Here you are"));
 
         }
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-
-    private void stopLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-        }
-        //mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-    }
-
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mGoogleApiClient.isConnected()) {
-            startLocationUpdates();
-        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        updateUI();
+        map.addMarker(new MarkerOptions().position(getLocationFromAddress(mContext,"Hybenvej 133. Horsens 8700, Denmark")).title("Hybenvej 133. Horsens 8700, Denmark"));
+
     }
 
     public void click(View v) {
@@ -305,17 +203,55 @@ public class MainActivity extends AppCompatActivity implements android.location.
                     }
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return location;
     }
+    public LatLng getLocationFromAddress(Context context,String strAddress) {
 
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
 
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
 
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
 
+        } catch (IOException ex) {
 
+            ex.printStackTrace();
+        }
 
+        return p1;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
